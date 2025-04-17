5c22543f01edb429fb6a0bf9d57207b2d0f151df
KaistenYT <137458086+KaistenYT@users.noreply.github.com>
2025-04-17 11:29:21 -0400

    uso de ejs

--- crud-express/server.js
+++ crud-express/server.js
@@ -5,11 +5,23 @@ async function startServer() {
  
   const propietarioRouter = require('./routes/propietarioRouter')
   const RecepcionRouter = require('./routes/recepcionRoutes')
+  const cors = require('cors')
+
+  
 
   const app = express();
   const port = 3000;
 
+
+  app.use(cors())
   app.use(bodyParser.json());
+
+  app.set('view engine', 'ejs')
+
+  app.get('/', (req, res)=>{
+    res.render('pages/index.ejs')
+  })
+
 
   app.use('/api', propietarioRouter)
   app.use('/api', RecepcionRouter)
